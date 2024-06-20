97074633c85453c32e719ad3fa570b9dcc63df4b
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-06-20 19:14:17 +0530

    added chaneel base  url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     baseURL: "http://localhost:3001",
-    channelBaseUrl: "http://locahost:3000",
+    channelBaseUrl: "http://localhost:3000",
     // Add other configurations here if needed
 };
 
